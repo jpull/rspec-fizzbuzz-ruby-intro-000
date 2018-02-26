cd7a2382d552cc71fc00245be9b66fc81418ecf2
Jerry P <jpull@jpull.com>
2018-02-26 00:36:24 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,17 +1,14 @@
 def fizzbuzz(number)
-  numb = number / 3.0
-  numint = number / 3
-  puts numb
-  puts numint
   if (number / 3 == number / 3.0) && (number / 5 == number / 5.0 )
     puts "FizzBuzz"
-  elsif number/3 == number/3
+  elsif number / 3 == number / 3.0
     puts "Fizz"
-  elsif number/5 == number/5
+  elsif number / 5 == number / 5.0
     puts "Buzz"
   else number
   end
 end
 
 puts "Enter Number"
-fizzbuzz(15)
+number = gets
+fizzbuzz(number)
